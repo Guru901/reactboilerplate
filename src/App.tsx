e8f7acb60c878b80b098e6149147cdc8c5e5409f
Guru <padhhle@gmail.com>
2024-02-12 22:39:05 +0530

    Routing and Tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
 
 export default App;
